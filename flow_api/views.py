47280be0ca563e247bfb71d82978daa1d64401b9
Callum Messiter <callum.messiter@gmail.com>
2023-06-12 16:21:49 +0100

    Add support for filtering via query params

--- flow_api/views.py
+++ flow_api/views.py
@@ -16,51 +16,61 @@ mock_madrs_s_mean_scores = {
     'q10': 1
 }
 
-# get mean for each question across all submissions by all patients
-@api_view(['GET'])
-def patients_historical_madrs_s_mean_scores(request):
-    return JsonResponse(data={
-        "patientId": 1,
-        "mean_scores": mock_madrs_s_mean_scores
-    })
-
 # for a given patient, get mean for each question across all their submissions
 @api_view(['GET'])
 def patient_historical_madrs_s_mean_scores(request, id):
     return JsonResponse(data={
-        "count": 3,
-        "data": [
-            { "patientId": 1, "mean_scores": mock_madrs_s_mean_scores },
-            { "patientId": 2, "mean_scores": mock_madrs_s_mean_scores },
-            { "patientId": 3, "mean_scores": mock_madrs_s_mean_scores },
-        ]
+        "count": 1,
+        "data": { 
+            "patientId": id, 
+            "historical_mean_scores": mock_madrs_s_mean_scores 
+        }
     })
 
-# all patients who responded with a certain score on a certain question
-def filter_patients_by_madrs_s_question_score(request):
+# get mean for each question across all submissions by all patients
+@api_view(['GET'])
+def patients_historical_madrs_s_mean_scores(request):
     return JsonResponse(data={
         "count": 3,
         "data": [
-            { "patientId": 2, "mean_scores": mock_madrs_s_mean_scores },
-            { "patientId": 3, "mean_scores": mock_madrs_s_mean_scores },
+            { "patientId": 1, "historical_mean_scores": mock_madrs_s_mean_scores },
+            { "patientId": 2, "historical_mean_scores": mock_madrs_s_mean_scores },
+            { "patientId": 3, "historical_mean_scores": mock_madrs_s_mean_scores },
         ]
     })
 
 # all patients, with each of their submissions, each with a total score and depression severity
 # sorted by total score and with the option to filter on a minimum and/or maximum total score
+@api_view(['GET'])
 def patients_historical_madrs_s_submissions(request):
     return JsonResponse(data={
         "count": 1,
+        "filters": [ 
+            { "minTotalScore": request.query_params.get('minTotalScore') or 0 }, 
+            { "maxTotalScore": request.query_params.get('maxTotalScore') or 60 } 
+        ],
         "data": [
             { 
                 "patientId": 1, 
                 "submissions": [ 
                     { 
-                        "completedAt": datetime.date.today(), 
+                        "completedAt": datetime.date.today(),
                         "total_score": 30, 
                         "depression_severity": "SEVERE_DEPRESSION" 
                     } 
                 ] 
             },
+        ]
+    })
+
+# all patients who responded with a certain score on a certain question
+@api_view(['GET'])
+def filter_patients_by_madrs_s_question_score(request):
+    return JsonResponse(data={
+        "count": 3,
+        "filters": [ { "test": 4 } ],
+        "data": [
+            { "patientId": 2 },
+            { "patientId": 3 },
         ]
     })
